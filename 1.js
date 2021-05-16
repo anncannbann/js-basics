74a85cb9e6ead1509c0443fc182f583a83662210
anncannbann <anncannbann@gmail.com>
2021-05-17 02:05:54 +0530

    Data Types

--- 1.js
+++ 1.js
@@ -1,5 +1,9 @@
 /** first basic program */
 
+
+
+/** Values and Variables.  */
+/*
 let js = "amazing";
       //if(js ==="amazing") alert ("JS is fun");
       console.log(40+23+23-100);
@@ -10,3 +14,29 @@ let js = "amazing";
 
       let a=100,b=12;
       console.log(a+b);
+
+*/
+
+/**Data Types -7*/
+/*
+      //1.number
+      let age=12
+
+      //string
+      let l ="j";
+
+      //bool
+      let hell = true;
+
+      //undefined
+      let be;
+
+      //null empty value
+
+      //symbol unique value that cant be changed
+      
+      //Bigint large numbers
+
+*/
+
+
