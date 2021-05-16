18b904942b88a030c4cd45b4f9868d0446e4e039
anncannbann <anncannbann@gmail.com>
2021-05-17 02:01:26 +0530

    Valuyes and VAriables

--- 1.js
+++ 1.js
@@ -1,6 +1,12 @@
 /** first basic program */
 
 let js = "amazing";
-      if(js ==="amazing") alert ("JS is fun");
+      //if(js ==="amazing") alert ("JS is fun");
       console.log(40+23+23-100);
-      
+
+      //console.log("Jonas");
+      let x = "Jonas";
+      console.log(x); 
+
+      let a=100,b=12;
+      console.log(a+b);
