f17d9d2c0867a085066ae4e2e10ac816ff7aebb3
anncannbann <anncannbann@gmail.com>
2021-05-17 02:37:38 +0530

    Dynamic Typing

--- 1.js
+++ 1.js
@@ -30,23 +30,44 @@ let js = "amazing";
 
       //undefined
       let be;
+      console.log(be);
 
       //null empty value
+      //gives obj typeof
 
-      //symbol unique value that cant be changed
+      //symbol - unique value that cant be changed
       
-      //Bigint large numbers
+
+      //Bigint -large numbers
 
 
       //typeof
       console.log(typeof 100);
 
-*/
 
 
+      //dynamic typing
       let j = true;
       console.log(typeof j)
       console.log(j)
 
       j = 'heckyes';
       console.log(typeof j)
+
+*/
+
+
+
+/* let,const and var  diff ways to declaring variables*/
+
+      //using let
+      let a = 1;
+
+      //using var
+
+      //const - value cannot be changed / immutable.
+      const year =2000;
+
+      //var - avoid this!!!!!
+      var age = 44;
+      age = 50;
