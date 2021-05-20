5c981b2983a0b5bf6b6799993e1e6f73cba185e3
anncannbann <anncannbann@gmail.com>
2021-05-20 20:43:53 +0530

    different types of Functions

---
+++ basics-part2/script.js
@@ -0,0 +1,30 @@
+//for strict mode
+'use strict';
+
+
+//functions intro
+/*
+function logger(name)
+{
+    console.log(`Hello ${name}`);
+
+}
+logger('User');
+logger('John');
+logger('Jane');
+
+*/
+
+//function calling and using return.
+/*
+function fruit(apples,orange){
+    console.log(apples,orange);
+    const juice = `Juice with ${apples} apples and ${orange} oranges`;
+    return juice;
+}
+
+const x = fruit(10,4);
+console.log(x);
+
+*/
+
