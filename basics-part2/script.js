fb8f915bdb1c80eca7b93e63faab95489e4a433f
anncannbann <anncannbann@gmail.com>
2021-05-24 20:06:44 +0530

    Function Calling Function

--- basics-part2/script.js
+++ basics-part2/script.js
@@ -15,25 +15,6 @@ logger('Jane');
 
 */
 
-//ARROW FUNCTION 1 line code
-/*
-const c = birth => 2021-birth;
-const x = c(1998);
-console.log(x);
-*/
-//arrow for multiple
- 
-const yearUntillRetirement = (birth,firstname) =>{
-    const age = 2020- birth;
-    const retire = 65-age;
-    //return retire;
-    return `${firstname} retires in ${retire} years`;
-}
-
-console.log(yearUntillRetirement(1998,'Bani'));
-console.log(yearUntillRetirement(1998,'Bani'));
-
-
 //function calling and using return.
 /*
 function fruit(apples,orange){
@@ -46,6 +27,48 @@ const x = fruit(10,4);
 console.log(x);
 console.log(fruit(9,8));
 */
+
+
+//ARROW FUNCTION 1 line code
+/*
+const c = birth => 2021-birth;
+const x = c(1998);
+console.log(x);
+*/
+
+
+//arrow for multiple - shoudlnt use. Complicated. 
+// Arrow function doesnt allow this keyword thats why should use normal functions instead.
+/* 
+const yearUntillRetirement = (birth,firstname) =>{
+    const age = 2020- birth;
+    const retire = 65-age;
+    //return retire;
+    return `${firstname} retires in ${retire} years`;
+}
+console.log(yearUntillRetirement(1998,'Bani'));
+*/
+
+
+//Function calling inside another.
+
+function fruit(apples,orange){
+    const applepieces = cutfruit(apples);
+    const orangepieces = cutfruit(orange);
+    
+    console.log(apples,orange);
+    const juice = `Juice with ${applepieces} apples and ${orangepieces} oranges`;
+    return juice;
+}
+function cutfruit(fruit){
+    return fruit*4;
+}
+
+
+console.log(fruit(2,3));
+
+
+
 
 //////////////////////////////////////
 // Coding Challenge #1
