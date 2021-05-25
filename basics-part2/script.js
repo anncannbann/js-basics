6ff5a763acbb051c94aa82b796fc25e5693c2d71
anncannbann <anncannbann@gmail.com>
2021-05-25 15:34:58 +0530

    Array Operations:push,pop and others.

--- basics-part2/script.js
+++ basics-part2/script.js
@@ -37,7 +37,7 @@ console.log(x);
 */
 
 
-//arrow for multiple - shoudlnt use. Complicated. 
+//arrow for multiple - shoudnt use. Complicated. 
 // Arrow function doesnt allow this keyword thats why should use normal functions instead.
 /* 
 const yearUntillRetirement = (birth,firstname) =>{
@@ -65,10 +65,83 @@ function cutfruit(fruit){
     return fruit*4;
 }
 
-
 console.log(fruit(2,3));
+*/
+
+
+//Arrays
+/*
+//way1
+const friends = ['Joey','Phoebe','Chandler','Ross'];
+console.log(friends);
+
+//way2
+const years = new Array(1998,2665,1111,5555,2525);
+
+//Manipulations
+console.log(years);
+console.log(years[years.length-1]);
+
+friends[friends.length] = 'Monica';
+console.log(friends);
+
+friends.pop();
+console.log(friends);
+
+
+friends[friends.length] =69;
+console.log(friends);
+
+friends[friends.length] = ['So','There','You','Go'];
+console.log(friends);
+
+
+
+const calcAge = function(birth){
+    return 2021 - birth;
+}
+const y = [1990,1992,1998,2000,1989,1969];
 
 */
+/*
+//array Operations
+
+const friends = ['Joey','Phoebe','Chandler','Ross'];
+console.log(friends);
+
+//Add
+friends.push('Monica');
+console.log(friends);
+
+//Puts the element added as the first!!!!???!!
+friends.unshift('Rachel');
+console.log(friends);
+
+//Removes the last.
+const popped = friends.pop();
+//Can return the popped element. Useful for some cases.
+console.log(popped)
+console.log(friends);
+
+//Removes First!!!!
+friends.shift();
+console.log(friends);
+
+
+//to check an element position
+console.log(friends.indexOf('Joey'));
+console.log(friends.indexOf('Rachel')); //Doesnt Exist.
+
+//es6 method for finding element = Returns True or False, uses strict. will not work fo rtype coehersion.
+friends.push(22);
+console.log(friends.includes('Joey'));
+console.log(friends.includes('Rachel'));
+
+//uses strict. will not work fo type coehersion.
+console.log(friends.includes('22')); //False
+console.log(friends.includes(22)); //True
+*/
+
 
 
 
