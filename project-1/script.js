ab4d865a329903c1c3eed940c0d212e37c4d2a9f
anncannbann <anncannbann@gmail.com>
2021-06-02 23:05:55 +0530

    Project-1: Guess My Number -Starter.

--- project-1/script.js
+++ project-1/script.js
@@ -1,3 +1,6 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// click on check to take input given by the user.
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+});
