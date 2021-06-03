620c25b8c9a9ad3884daca54888d8aad81eef4ce
anncannbann <anncannbann@gmail.com>
2021-06-03 18:59:21 +0530

    Project 2

--- project-1/script.js
+++ project-1/script.js
@@ -3,6 +3,7 @@
 //secret number
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
+
 //Score count
 let score = 10;
 let highscore = 0;
@@ -13,33 +14,25 @@ let highscore = 0;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
-  // console.log(
-  //   `guess ${guess} and secrtNumebr ${secretNumber} and typeof ${typeof guess}`
-  // );
-
   //Game Logic Begins Here.....
   //
   //No input and check button clicked.
   if (!guess) {
-    document.querySelector(
-      '.message'
-    ).textContent = `⚠️ No number entered 🙁  `;
+    displayMessage(`⚠️ No number entered 🙁  `);
     console.log(`No number entered.🙁 `);
   }
 
   //Correct Answer aka PLayer Winsssssssssss.
   else if (guess === secretNumber) {
-    document.querySelector(
-      '.message'
-    ).textContent = `You guessed My Number!🤯 `;
+    displayMessage(`You guessed My Number!🤯 `);
 
     document.querySelector('body').style.backgroundColor = '#fe4a49';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = secretNumber;
-    console.log(`score is ${score} and highscore is cuurently ${highscore}`);
     if (score > highscore) {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
+      console.log(`score is ${score} and highscore is cuurently ${highscore}`);
     }
   }
 
@@ -74,38 +67,23 @@ document.querySelector('.check').addEventListener('click', function () {
   //
   else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent =
-        guess > secretNumber ? `Too high. 📈` : `Too Low. 📉`;
+      // document.querySelector('.message').textContent =
+      //   guess > secretNumber ? `Too high. 📈` : `Too Low. 📉`;
+      displayMessage(guess > secretNumber ? `Too high. 📈` : `Too Low. 📉`);
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector(
-        '.message'
-      ).textContent = `You Lost the Game 😈  Try again Next Time.`;
+      displayMessage(`You Lost the Game 😈  Try again Next Time.`);
       document.querySelector('.score').textContent = 0;
     }
   }
 });
 
-///////////////////////////////////////
-// Coding Challenge #1
-
-/* 
-Implement a game rest functionality, so that the player can make a new guess! Here is how:
-
-1. Select the element with the 'again' class and attach a click event handler
-2. In the handler function, restore initial values of the score and secretNumber variables
-3. Restore the initial conditions of the message, number, score and guess input field
-4. Also restore the original background color (#222) and number width (15rem)
-
-GOOD LUCK 😀
-*/
-
 document.querySelector('.again').addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   score = 10;
 
-  document.querySelector('.message').textContent = 'Start Guessing...';
+  displayMessage('Start Guessing...');
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
@@ -113,3 +91,8 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
   console.log(secretNumber);
 });
+
+//Reducing Repetitive Code by Making a function for it.
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
