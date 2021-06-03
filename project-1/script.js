58ea8b82430567b266f33cf714e60f52fb39e540
anncannbann <anncannbann@gmail.com>
2021-06-03 15:33:39 +0530

    Implementing click on the button and putting conditions for diff scenarios.

--- project-1/script.js
+++ project-1/script.js
@@ -2,5 +2,11 @@
 
 // click on check to take input given by the user.
 document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
+  const guess = document.querySelector('.guess').value;
+  if (!guess) {
+    document.querySelector(
+      '.message'
+    ).textContent = `⚠️ No number entered 🙁  `;
+    console.log(`No number entered.🙁 `);
+  }
 });
