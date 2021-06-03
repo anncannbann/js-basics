95fca5b51f8812d4a0082ed9e4aed77f0ee6fe82
anncannbann <anncannbann@gmail.com>
2021-06-03 16:56:10 +0530

    Coding Challenge

--- project-1/script.js
+++ project-1/script.js
@@ -6,8 +6,7 @@ const secretNumber = Math.trunc(Math.random() * 20) + 1;
 //Score count
 let score = 5;
 
-//to display number =>
-document.querySelector('.number').textContent = secretNumber;
+//to display number => document.querySelector('.number').textContent = secretNumber;
 
 // click on check to take input given by the user.
 document.querySelector('.check').addEventListener('click', function () {
@@ -18,20 +17,25 @@ document.querySelector('.check').addEventListener('click', function () {
   // );
 
   //Game Logic Begins Here.....
-  //Empty Check.
+  //No input and check button clicked.
   if (!guess) {
     document.querySelector(
       '.message'
     ).textContent = `⚠️ No number entered 🙁  `;
     console.log(`No number entered.🙁 `);
   }
-  //Correct Answer.
+
+  //Correct Answer aka PLayer Winsssssssssss.
   else if (guess === secretNumber) {
     document.querySelector(
       '.message'
     ).textContent = `You guessed My Number!🤯 `;
     document.querySelector('.highscore').textContent = score;
+    document.querySelector('body').style.backgroundColor = '#fe4a49';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
   }
+
   //Number is Higher than the Secret number
   else if (guess > secretNumber) {
     if (score > 1) {
@@ -60,3 +64,17 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+
+GOOD LUCK 😀
+*/
