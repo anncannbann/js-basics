4874ef11f94df48cac6421b682b3a9d372414249
anncannbann <anncannbann@gmail.com>
2021-06-07 17:36:04 +0530

    working with query selcter all

--- project-2/script.js
+++ project-2/script.js
@@ -1,1 +1,12 @@
-"use strict";
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClose = document.querySelector('.close-modal');
+
+//Node list, becausw we have three btns in the same class.
+//IF we use query sElector simply, it will only select the first one.
+const btnsOpen = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnsOpen.length; i++) {
+  console.log(btnsOpen[i].textContent);
+}
